dc447c99c83fdbf9288b0a11543f4ff6fae9147b
Joshua Burger <joshberger6969@outlook.com>
2021-09-19 21:43:20 -0400

    New PhotoCard component rendering a <h1>Hello</h1>

---
+++ src/components/PhotoCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class PhotoCard extends React.Component {
+
+    
+
+    render() {
+        return(
+            <h1>Hello</h1>
+        )
+    }
+        
+    
+
+
+}
+
+export default PhotoCard;
