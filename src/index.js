6058154b5093d25a919799c521d0c0737a5bd348
Joshua Burger <joshberger6969@outlook.com>
2021-09-19 21:46:38 -0400

    import NasaReducer, import applymiddleware, createStore, compose from redux, import thunk from redux-thunk, Create store using createStore from redux.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import NasaReducer from './reducers/NasaReducer';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(NasaReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +18,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
