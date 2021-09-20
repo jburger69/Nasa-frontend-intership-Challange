f22e7c6b7e05e99bc8635bd9b2d4975d34b7e08d
Joshua Burger <joshberger6969@outlook.com>
2021-09-19 21:39:14 -0400

    Change function App into a Class component to have access to lifecycles, import getPhotos from actions folder to fetch from nasa API, Inside of componentDidMount I make a call to the action getPhotos, Render PhotoCard component with props passing down, MapStateToProps function to return photos as props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+import { connect } from 'react-redux';
+import { getPhotos } from './actions/NasaPhotos';
+
+import PhotoCard from './components/PhotoCard';
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getPhotos();
+  }
+
+  render() {
+    return(
+      <div>
+        <PhotoCard  photos={this.props.photos}/>
+      </div>
+    )
+  }
+
+
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+  return {
+    photos: state.photos,
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps, { getPhotos })(App);
