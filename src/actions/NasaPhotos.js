19e7e70a531ee8eca253167852b29fd17b1893be
Joshua Burger <joshberger6969@outlook.com>
2021-09-21 21:52:10 -0400

    Fix fetch to lower image count

--- src/actions/NasaPhotos.js
+++ src/actions/NasaPhotos.js
@@ -1,6 +1,6 @@
 export const getPhotos = () => {
     return (dispatch) => {
-        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=jiUh9sloyTBT6HvUQjWJEP0bOk6vvgSUEKHaPv7t")
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=jiUh9sloyTBT6HvUQjWJEP0bOk6vvgSUEKHaPv7t")
             .then(resp => resp.json())
             .then(photos => {
                 dispatch({
